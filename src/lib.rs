b7d9112677cc858f7d6252b4968021132c40a98d
David Wheeler <dwheeler20874@gmail.com>
2025-10-25 19:24:48 -0400

    z_split is actually working now. (I'm pretty sure.)
    Code is messy and suboptimal but results check out.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod split;
 
 // use vector3d::Vector3d;
-use vecmath::{Vector3, vec3_normalized};
-use quaternion::{rotation_from_to, rotate_vector, axis_angle, mul, dot};
+use vecmath::{Vector3, vec3_normalized, vec3_dot};
+use quaternion::{rotation_from_to, rotate_vector, axis_angle, mul};
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
@@ -23,7 +23,7 @@ pub fn midpoint(a: Vector3<f64>, b: Vector3<f64>) -> Vector3<f64> {
     mid
 }
 
-const THETA_ACCURACY: f64 = 0.0001_f64.to_radians();
+const THETA_ACCURACY: f64 = 0.0000001_f64.to_radians();
 
 // We need to find a point, c, to the left of a->b that we can reach in some distance, theta,
 // then turn 60 degrees right, and then theta/2 forward to reach b.
@@ -32,24 +32,35 @@ pub fn z_point(a: Vector3<f64>, b: Vector3<f64>) -> Vector3<f64> {
     // Let theta_ab be the distance from a to b.
     // Strategy: Do a binary search between theta_ac and theta_ac/2
     let q_ab = rotation_from_to(a, b);
-    let theta_ab = 2.0 * q_ab.0.acos();
+    let theta_ab = vec3_dot(a, b).acos();
     let axis_ab = vec3_normalized(q_ab.1);
+    // println!("axis_ab: {:?}", axis_ab);
     let axis_a = vec3_normalized(a);
+    // println!("axis_a: {:?}", axis_a);
 
     let mut theta_low = theta_ab/4.0;
     let mut theta_high = theta_ab;
     while theta_high-theta_low > THETA_ACCURACY {
+        // println!("Range: {} deg - {} deg", theta_low.to_degrees(), theta_high.to_degrees());
+
         let theta_test = (theta_high+theta_low)/2.0;
 
-        // Rotate by theta_test about Z.
+        // Rotate by theta_test about axb axis
         let q1 = axis_angle(axis_ab, theta_test);
-        // Turn -60 degrees about X.
-        let q2 = axis_angle(axis_a, (60.0_f64).to_radians());
-        // Rotate by theta_test/2 about Z.
+        // Turn -60 degrees about a axis
+        let q2 = axis_angle(axis_a, (-60.0_f64).to_radians());
+        // Rotate by theta_test/2 about axb axis
         let q3 = axis_angle(axis_ab, theta_test/2.0);
         
-        let q = mul(q3, mul(q2, q1));
-        let theta_result = q.0.acos()/2.0;
+        let q = mul(mul(q1, q2), q3);
+        let result = rotate_vector(q, a);
+
+        let theta_result = vec3_dot(a, result).acos();
+        // println!("theta_test {} -> Theta_result = {} deg", 
+        //    theta_test.to_degrees(), 
+        //    theta_result.to_degrees());
+        // println!("rotation 1: {} deg, rotation 2: {} deg, rotation 3: {} deg",
+        //    2.0*q1.0.acos().to_degrees(), 2.0*q2.0.acos().to_degrees(), 2.0*q3.0.acos().to_degrees());
 
         if theta_result > theta_ab {
             theta_high = theta_test;
@@ -57,28 +68,40 @@ pub fn z_point(a: Vector3<f64>, b: Vector3<f64>) -> Vector3<f64> {
         else {
             theta_low = theta_test;
         }
+        
     }
     let theta_ac = (theta_high+theta_low)/2.0;
 
-    println!("Theta ac found as {} degrees", theta_ac.to_degrees());
+    // println!("Theta ac found as {} degrees", theta_ac.to_degrees());
 
-    // Rotate by theta_test about Z.
+    // Rotate by theta_test about axb
     let q1 = axis_angle(axis_ab, theta_ac);
-    // Turn -60 degrees about X.
-    let q2 = axis_angle(axis_a, (60.0_f64).to_radians());
-    // Rotate by theta_test/2 about Z.
+    // Turn -60 degrees about a
+    let q2 = axis_angle(axis_a, (-60.0_f64).to_radians());
+    // Rotate by theta_test/2 about axb
     let q3 = axis_angle(axis_ab, theta_ac/2.0);
     
-    let q = mul(q1, mul(q2, q3));
+    let q = mul(mul(q1, q2), q3);
+    let result = rotate_vector(q, a);
+    let q_result = rotation_from_to(a, result);
 
-    // Find angle between current ab and q
-    let phi = dot(q_ab, q).acos();
-    println!("  Phi is {phi} ({} degrees)", phi.to_degrees());
+    // Find angle between current a->b and a->result
+    let phi = vec3_dot(vec3_normalized(q_ab.1), vec3_normalized(q_result.1)).acos();
+    // println!("  Phi is {phi} ({} degrees)", phi.to_degrees());
 
     // Now, to get to c, we need to turn by phi around axis a, then q1.
     let q_phi = axis_angle(a, phi);
-    let q_c = mul(q_phi, q1);
+    let q_c = mul(q_phi, q1); // q1 * q_phi?
     let c = rotate_vector(q_c, a);
+
+    // let q_full = mul(q_phi, q_result);
+    // let end = rotate_vector(q_full, a);
+    // println!("Full operation got to: {:?}", end);
+
+    // Check angles from a to c, b to c
+    // let ac = vec3_dot(a, c).acos();
+    // let bc = vec3_dot(b, c).acos();
+    // println!("ac : {} deg, bc : {} deg", ac.to_degrees(), bc.to_degrees());
 
     c
 }
@@ -95,6 +118,7 @@ pub fn z_split(a: Vector3<f64>, b: Vector3<f64>) -> (Vector3<f64>, Vector3<f64>)
 #[cfg(test)]
 mod tests {
     use super::*;
+    use vecmath::{Vector3, vec3_normalized, vec3_dot};
 
     #[test]
     fn it_works() {
@@ -113,11 +137,35 @@ mod tests {
 
     #[test]
     fn test_split() {
-        let a: Vector3::<f64> = [1.0, 0.0, 0.0];
-        let b: Vector3::<f64> = [0.0, 1.0, 0.0];
+        let a: Vector3::<f64> = vec3_normalized([1.0, 0.0, 0.0]);
+        let b: Vector3::<f64> = vec3_normalized([0.0, 1.0, 0.0]);
         let (c, d) = z_split(a, b);
+
+        let theta_ac = vec3_dot(a, c).acos();
+        let theta_cd = vec3_dot(c, d).acos();
+        let theta_db = vec3_dot(d, b).acos();
+
+        println!("thetas ac: {}, cd: {}, db: {}", 
+            theta_ac.to_degrees(), theta_cd.to_degrees(), theta_db.to_degrees());
+
+        assert!((theta_ac-theta_cd).abs() <= THETA_ACCURACY);
+        assert!((theta_cd-theta_db).abs() <= THETA_ACCURACY);
+        assert!((theta_db-theta_ac).abs() <= THETA_ACCURACY);
+
 
         println!("c: {c:?}");
         println!("d: {d:?}");
+    }
+
+    #[test]
+    fn test_q_mul() {
+        let x90 = axis_angle([1.0, 0.0, 0.0], 90.0_f64.to_radians());
+        let y90 = axis_angle([0.0, 1.0, 0.0], 90.0_f64.to_radians());
+
+        let q1q2 = mul(x90, y90); // computes y90 * x90, rot about x, then y
+        let q2q1 = mul(y90, x90);
+
+        println!("q1 {x90:?} * q2 {y90:?} = {q1q2:?}");
+        println!("q2 {y90:?} * q1 {x90:?} = {q2q1:?}");   
     }
 }
